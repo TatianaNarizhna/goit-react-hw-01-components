7d7cb708686a41f771d2a959351d5868b7759925
TatianaNarizhna <narizhna.tania@gmail.com>
2021-07-06 18:40:30 +0300

    task1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Profile from './components/social-profile/Social-profile';
+import user from './data/user.json';
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/social-profile/Social-profile.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = ({ name, tag, location, avatar, stats }) => {
+  return (
+    <div>
+      <div>
+        <img src={avatar} alt={name} />
+        <p>{name}</p>
+        <p>{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
